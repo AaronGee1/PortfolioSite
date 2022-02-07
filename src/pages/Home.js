224f329cf20a9a5221e444cf6002bff328aacfec
AaronGee1 <aaron.gee@ryerson.ca>
2022-02-06 23:00:17 -0500

    Added a projects component that will contain cards of each project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import ProfileJumbotron from "../components/ProfileJumbotron";
 import Experience from "../components/Experience";
 import Education from "../components/Education";
+import Projects from "../components/Projects";
 
 const Home = () => {
   return (
     <div>
       <ProfileJumbotron />
+      <Projects />
       <Experience />
       <Education />
     </div>
